1f21da8531858c8053a024343d25ab6a17aaa09e
H51M14\Student <Student@gmail.com>
2024-08-21 08:11:52 +0700

    Test3

--- JavaScript_Fundamentals.js
+++ JavaScript_Fundamentals.js
@@ -1,1 +1,24 @@
 console.log()
+var Mark_w_input = document.getElementById('Mark_w');
+var Mark_h_input = document.getElementById('Mark_h');
+var John_w_input = document.getElementById('John_w');
+var John_h_input = document.getElementById('John_h');
+var result = document.getElementById('result');
+var cacul = document.getElementById('cacul');
+var MarkHigherBMI = 0;
+
+function BMI() {
+    var Mark_w = parseFloat(Mark_w_input.value) || 0;
+    var Mark_h = parseFloat(Mark_h_input.value) || 0;
+    var John_w = parseFloat(John_w_input.value) || 0;
+    var John_h = parseFloat(John_h_input.value) || 0;
+    var Mark_BMI = Mark_w / Mark_h ** 2;
+    var John_BMI = John_w / John_h ** 2;
+    if (Mark_BMI > John_BMI) MarkHigherBMI = 1;
+    if (MarkHigherBMI == 1) {
+        result.innerText = 'Mark co BMI cao hon';
+    } else {
+        result.innerText = 'John co BMI cao hon';
+    }
+}
+cacul.addEventListener('click', BMI);
