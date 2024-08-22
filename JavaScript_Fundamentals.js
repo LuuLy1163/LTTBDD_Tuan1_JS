f275062129b29eeac9c22eb079315499c3adbd4a
LuuLy111 <ly@xem.edu.vn>
2024-08-23 00:10:50 +0700

    Bai2

--- JavaScript_Fundamentals.js
+++ JavaScript_Fundamentals.js
@@ -13,13 +13,16 @@ window.onload = function() {
         var Mark_h = parseFloat(Mark_h_input.value) || 0;
         var John_w = parseFloat(John_w_input.value) || 0;
         var John_h = parseFloat(John_h_input.value) || 0;
-        var Mark_BMI = Mark_w / Mark_h ** 2;
-        var John_BMI = John_w / John_h ** 2;
+        var Mark_BMI = (Mark_w / Mark_h ** 2);
+        var John_BMI = (John_w / John_h ** 2);
         if (Mark_BMI > John_BMI) MarkHigherBMI = 1;
+        else if (Mark_BMI == John_BMI) MarkHigherBMI = 2;
         if (MarkHigherBMI == 1) {
-            result.innerText = 'Mark co BMI cao hon';
+            result.innerText = "Mark's BMI(" +Mark_BMI.toFixed(2)+ ") is higher than John's("+John_BMI.toFixed(2) + ")";
+        } else if (MarkHigherBMI == 2) {
+            result.innerText = "John's BMI(" +John_BMI.toFixed(2)+ ") is equal to Mark's("+Mark_BMI.toFixed(2) + ")";
         } else {
-            result.innerText = 'John co BMI cao hon';
+            result.innerText = "John's BMI(" +John_BMI.toFixed(2)+ ") is higher than Mark's("+Mark_BMI.toFixed(2) + ")";
         }
     }
     calcul.addEventListener('click', BMI);
